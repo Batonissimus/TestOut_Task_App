d40b29f93faf79f4c69c3b36ff035fbc72b745db
Batonissimus <Batonissimus@gmail.com>
2020-09-22 18:47:05 -0600

    Initial commit

---
+++ src/com/TestOut/Main.java
@@ -0,0 +1,26 @@
+package com.TestOut;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        Test1 result1 = new Test1();
+      /*  if (result1.PortCheck().equals("Your Score: 1 of 1 (100%)"))
+        {
+            System.out.println("We are good!");
+        }
+        else
+        {
+            System.out.println("We will be good later!");
+        }*/
+
+
+    }
+}
